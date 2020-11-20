e9686406b29ac12f9e7399a0c666098a5f13d10c
Dany Trudel <dany.trudel20@gmail.com>
2020-11-20 10:34:55 -0500

    debut

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 
+// des que le contenu est monté, le script va commencer
+
 document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    // le jeu
+
+    class Jeu {
+
+        constructor(){
+            console.log("AAAAAAAAAAAA")
+        }
+
+    }
+
+    //le serpent
+
+    class Serpent {
+
+
+        constructor() {
+            console.log("BBBBBBBBBBBBB")
+        }
+    }
+
+
+    // la pomme
+    class Pomme {
+
+
+        constructor() {
+            console.log("CCCCCCCCCCCCCCCCC")
+        }
+    }
+
+
+    var unePartie = new Jeu();
+
 
 
 });
