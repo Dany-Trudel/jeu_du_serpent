b660a983401bfd14a09ac6c9051d40565ab5a616
Dany Trudel <dany.trudel20@gmail.com>
2020-11-20 11:26:07 -0500

    deplacement du serpent en processus

--- js/script.js
+++ js/script.js
@@ -23,17 +23,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         nouvellePartie(){
 
+            this.finPartie();
+
             this.affichagePointage(1);
 
-            this.pomme = new Pomme();
+            this.pomme = new Pomme(this);
 
-            this.serpent = new Serpent();
+            this.serpent = new Serpent(this);
 
 
         }
 
         finPartie(){
 
+            if(this.pomme !== undefined){
+            this.pomme.supprimePomme();
+            this.pomme = undefined;
+        }
         }
 
         affichagePointage(_lePointage){
@@ -50,9 +56,79 @@ document.addEventListener("DOMContentLoaded", function(event) {
     class Serpent {
 
 
-        constructor() {
+        constructor(_leJeu) {
             console.log("BBBBBBBBBBBBB")
+
+            this.leJeu = _leJeu;
+
+
+            this.currentX = -1;
+            this.currentY= 0;
+            this.nextMoveX = 1;
+            this.nextMoveY = 0;
+
+            this.serpentLongueur = 1;
+            this.tblCarreSerpent = [];
+
+            this.vitesse = 250;
+            this.timing = setInterval(this.controleSerpent.bind(this), this.vitesse)
+
+
+            document.addEventListener("keydown", this.verifTouche.bind(this))
         }
+
+        verifTouche(_evt){
+
+            var evt = _evt
+            console.log(evt.keyCode);
+
+            this.deplacement(evt.keyCode)
+        }
+
+        deplacement(dirCode){
+
+            switch(dirCode){
+                case 37:
+                    this.nextMoveX = -1;
+                    this.nextMoveY = 0;
+                    break;
+                case 38:
+                    this.nextMoveX = 0;
+                    this.nextMoveY = -1;
+                    break;
+                case 39:
+                    this.nextMoveX = 1;
+                    this.nextMoveY = 0;
+                    break;
+                case 40:
+                    this.nextMoveX = 0;
+                    this.nextMoveY = 1;
+                    break;
+
+
+            }
+            //console.log(this.nextMoveX, this.nextMoveY);
+        }
+
+        controleSerpent(){
+            var nextX = this.currentX + this.nextMoveX;
+            var nextY = this.currentY + this.nextMoveY;
+
+            this.dessineCarre(nextX, nextY);
+            this.currentX = nextX;
+            this.currentY = nextY;
+
+        }
+
+        dessineCarre(x, y){
+
+
+        }
+
+        supprimeSerpent() {
+
+    }
+
     }
 
 
@@ -60,9 +136,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     class Pomme {
 
 
-        constructor() {
+        constructor(_leJeu) {
             console.log("CCCCCCCCCCCCCCCCC")
+
+            this.leJeu = _leJeu;
+
+            this.pomme = [];
+
+            this.ajoutePomme();
+
         }
+
+        ajoutePomme(){
+            var posX = Math.floor(Math.random() * this.leJeu.grandeurGrille);
+            var posY = Math.floor(Math.random() * this.leJeu.grandeurGrille);
+
+            this.pomme = [this.leJeu.s.rect(posX * this.leJeu.grandeurCarre, posY * this.leJeu.grandeurCarre, this.leJeu.grandeurCarre, this.leJeu.grandeurCarre).attr({fill: 'red'}), posX, posY];
+
+        }
+        supprimePomme(){
+            this.pomme[0].remove();
+        }
+
+
     }
 
 
